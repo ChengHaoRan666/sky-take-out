811e0a18423cbf7ac23fe69d68b04575a6c5bf08
ChengHaoRan <2640708884@qq.com>
2024-11-13 17:00:45 +0800

    第一天完成

--- sky-common/src/main/java/com/sky/constant/AutoFillConstant.java
+++ sky-common/src/main/java/com/sky/constant/AutoFillConstant.java
@@ -7,8 +7,8 @@ public class AutoFillConstant {
     /**
      * 实体类中的方法名称
      */
-    public static final String SET_CREATE_TIME = "setCreateTime";
-    public static final String SET_UPDATE_TIME = "setUpdateTime";
-    public static final String SET_CREATE_USER = "setCreateUser";
-    public static final String SET_UPDATE_USER = "setUpdateUser";
+    public static final String SET_CREATE_TIME = "setCreateTime"; // 设置创建时间
+    public static final String SET_UPDATE_TIME = "setUpdateTime"; // 设置更新时间
+    public static final String SET_CREATE_USER = "setCreateUser"; // 设置创建用户
+    public static final String SET_UPDATE_USER = "setUpdateUser"; // 设置更新用户
 }
